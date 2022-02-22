e7fe5dcc58b89e54398a11b311b6a200b1ffb116
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-22 17:31:06 +0100

    aggiunti file

---
+++ myscript.js
@@ -0,0 +1,4 @@
+// Visualizzare in pagina 5 numeri casuali
+// Dopo la chiusura dell’alert parte un timer di 30 secondi.
+// Alla fine dei 30 secondi l’utente dovrà inserire, uno alla volta, i numeri che ha visto precedentemente
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri sono stati individuati.
